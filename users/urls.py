dc0527343cd17ff8088486ae361292aae7b306cc
Diana <lix_diana@yahoo.com>
2021-05-09 19:06:24 +0300

    before change in users app folder structure

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from users.views import register_view
+
+app_name = 'users'
 
 # localhost:8000/users
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html')),
     path('', include('django.contrib.auth.urls')),
+    path('register/', register_view, name='register'),
 ]
