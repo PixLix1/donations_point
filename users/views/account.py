38451c48c8f5c1d1a22e936205af38fd8fd175f8
Diana <lix_diana@yahoo.com>
2021-05-09 23:09:31 +0300

    user registration model - add to db

--- users/views/account.py
+++ users/views/account.py
@@ -1,25 +1,21 @@
 from django.shortcuts import render, redirect, reverse
-# from users.forms import RegisterForm
+from users.forms import RegisterForm
+# from django.contrib.auth.forms import UserCreationForm as RegisterForm  #can be used as is but relays on username only
 # from users.emails import send_register_email
-#
-#
-# # Create your views here.
-# def register_view(request):
-#     if request.method == 'GET':
-#         form = RegisterForm()
-#     else:  # consider this is POST method only
-#         form = RegisterForm(request.POST)
-#
-#         if form.is_valid():
-#             user = form.save()
-#             send_register_email(user)
-#             return redirect(reverse('users:account:login'))
-#
-#     return render(request, 'users/register.html', {
-#         'form': form
-#     })
 
 
-# # Create your views here.
+# Create your views here.
 def register_view(request):
-    return render(request, 'register.html', {})
+    if request.method == 'GET':
+        form = RegisterForm()
+    else:  # consider this is POST method only
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            # send_register_email(user)
+            return redirect(reverse('users:account:login'))
+
+    return render(request, 'users/register.html', {
+        'form': form
+    })
